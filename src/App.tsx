be05bc90b8198e6684d1f224b3eb5080bd559d9c
ToeMyint Aung <clonejack.aung679@gmail.com>
2022-02-17 14:35:01 +0900

    Flex_Test (#1)
    * firststep
    
    * 2ndstep
    
    * flexadd
    
    * allflexboxexamples
    
    * addvstack
    
    * allcomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,58 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
+  Flex,
+  Spacer,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Grid,
+  HStack,
+  VStack,
+} from "@chakra-ui/react";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
+    <Box>
+      <Text>Flex and Spacer: Full width, equal Spacing</Text>
+      <Flex>
+        <Box w="70px" h="10" bg="red.500" />
+        <Box w="170px" h="10" bg="red.500" />
+        <Box w="180px" h="10" bg="red.500" />
+      </Flex>
+
+      <Text>
+        Grid: The children start at the beginning, the 1/3 mark and 2/3 mark
+      </Text>
+      <Grid templateColumns="repeat(3, 1fr)" gap={3}>
+        <Box w="170px" h="10" bg="blue.500" />
+        <Box w="170px" h="10" bg="blue.500" />
+        <Box w="170px" h="10" bg="blue.500" />
       </Grid>
+
+      <Text>
+        HStack: The children have equal spacing but don't span the whole
+        container
+      </Text>
+      <HStack spacing="24px" justifyContent={"center"} alignItems={"center"}>
+        <Box w="70px" h="10" bg="teal.500"> ONE </Box>
+        <Box w="170px" h="10" bg="teal.500" > TWO </Box>
+        <Box w="180px" h="10" bg="teal.500" > THREE</Box>
+      </HStack>
+      <VStack spacing="24px">
+        <Box w="170px" h="10" p="2" bg="pink.500" >HELLO TESTING</Box>
+        <Box w="170px" h="10" bg="pink.500" />
+        <Box w="170px" h="10" bg="pink.500" />
+      </VStack>
+      <Flex>
+        <Box p="4" bg="red.400">
+          LeftFlexBox
+        </Box>
+        <Spacer />
+        <Box p="4" bg="green.400">
+          RightFlexBox
+        </Box>
+      </Flex>
     </Box>
   </ChakraProvider>
-)
+);
